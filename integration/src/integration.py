04705b40f21cdfda012d0756987853a6723257ba
David Schroeder <david@Davids-MacBook-Pro.local>
2025-02-17 09:19:09 -0700

    changed integration.py to only acccept book_pictures return value from extract_books func call

--- integration/src/integration.py
+++ integration/src/integration.py
@@ -7,7 +7,7 @@ from get_book_data import get_book_details
 def getBookTitles(pictures):
     # pictures is a file path
 
-    book_pictures = extract_books(pictures)
+    _, book_pictures = extract_books(pictures)
 
     results_words = []
     for book_picture in book_pictures:
